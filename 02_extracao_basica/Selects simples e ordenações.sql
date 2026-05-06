95b21eca355789a23b4e8d511cf11f4b6bd029a6
Arthur Entsev <126127157+arthurlynow098@users.noreply.github.com>
2026-05-06 09:08:07 -0300

    Add files via upload

---
+++ 02_extracao_basica/Selects simples e ordenações.sql
@@ -0,0 +1,9 @@
+// 2.1, 2.2 e 2.3 - Selects simples e ordenações
+
+2.1 select nome_obra, orcamento from projetos;
+
+2.2 select * from projetos 
+order by nome_obra ASC;
+
+2.3 select nome_obra, ano_inicio from projetos
+order by ano_inicio desc;
